c7f967abf173c5ff3544bf6840d3c509a520d7fe
Sourabh_g_ <saurabhbante11@gmail.com>
2026-03-17 05:58:33 +0530

    new update ignore main file update

--- backend/src/main/java/com/example/skillswap/config/SecurityConfig.java
+++ backend/src/main/java/com/example/skillswap/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.skillswap.config;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -56,13 +57,36 @@ public class SecurityConfig {
     }
 
     // CORS configuration
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//
+//        configuration.setAllowedOrigins(Arrays.asList(
+//                "http://localhost:5173", "http://localhost:5174", "https://my-skill-swap.vercel.app"
+//        ));
+//
+//        configuration.setAllowedMethods(Arrays.asList(
+//                "GET", "POST", "PUT", "DELETE", "OPTIONS"
+//        ));
+//
+//        configuration.setAllowedHeaders(Arrays.asList("*"));
+//        configuration.setAllowCredentials(true);
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//
+//        return source;
+//    }
+    
+    @Value("${cors.allowed.origins}")
+    private String corsOrigins;
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
+
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(Arrays.asList(
-                "http://localhost:5173", "http://localhost:5174", "https://my-skill-swap.vercel.app"
-        ));
+        configuration.setAllowedOrigins(Arrays.asList(corsOrigins.split(",")));
 
         configuration.setAllowedMethods(Arrays.asList(
                 "GET", "POST", "PUT", "DELETE", "OPTIONS"
